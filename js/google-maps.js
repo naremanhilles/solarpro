d5625f12cf1d77591089783af7ad7c6ecb1ac580
Nareman <naremanmohhilles@gmail.com>
2020-09-07 17:44:24 +0300

    solar pro project

---
+++ js/google-maps.js
@@ -0,0 +1,197 @@
+// Google maps pin tooltip	
+	var markers = [
+    	{
+    	    "lat": '31.9147985',
+    	    "lng": '35.8858543',
+		  
+			"description": 'asalehlak'
+        }
+	];
+	
+	
+	// Google maps main api
+	window.onload = function () {
+		var mapOptions = {
+			center: new google.maps.LatLng(markers[0].lat, markers[0].lng),
+        	zoom: 13,
+			flat: true,
+			scrollwheel:false,
+			panControl:false,
+			zoomControl:true,
+			streetViewControl: false,
+			mapTypeControl: false,
+		
+	// Google maps style	
+           	styles: [
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#6195a0"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#e6f3d6"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 45
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#f4d2c5"
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "labels.text",
+        "stylers": [
+            {
+                "color": "#4e4e4e"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#f4f4f4"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#787878"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#eaf6f8"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#eaf6f8"
+            }
+        ]
+    }
+],
+           	mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+			var infoWindow = new google.maps.InfoWindow();
+			var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+			for (i = 0; i < markers.length; i++) {
+			var data = markers[i]
+           	var myLatlng = new google.maps.LatLng(data.lat, data.lng);
+			var marker = new google.maps.Marker({
+           	position: myLatlng,
+           	map: map,
+        	icon: 'images/map-pin.svg',
+           	title: data.title
+           	});
+			(function (marker, data) {
+           	google.maps.event.addListener(marker, "click", function (e) {
+              	infoWindow.setContent(data.description);
+              	infoWindow.open(map, marker);
+              });
+            })(marker, data);
+        }
+    };
